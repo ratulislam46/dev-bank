543cb984c4b951a21886829057a6e1d8bffeb301
ratulislam46 <arfanratul46@gmail.com>
2025-03-02 01:45:24 +0600

    now button working

---
+++ script/activity.js
@@ -0,0 +1,12 @@
+
+// const taskTitle = document.querySelectorAll(".task-title");
+// const convartTitle = taskTitle[0].innerText;
+// console.log(convartTitle)
+
+
+
+document.getElementById("clear-history").addEventListener("click", function(event){
+    event.preventDefault();
+    const details = document.getElementById("activity-history").classList.add('hidden')
+    return details;
+})
--- script/utility.js
+++ script/utility.js
@@ -6,5 +6,34 @@ function getInnerValueById(id){
 }
 
 
+document.addEventListener("DOMContentLoaded" , function(){
+    let taskNumber = 6;
+    let checkBoxNumber = 12;
+    document.querySelectorAll("#compelete-btn").forEach(button =>{
+        button.addEventListener("click", function(event){
+            alert("Board update successfully")
+            taskNumber--;
+            document.getElementById("task-number").innerText = taskNumber;
+            checkBoxNumber ++;
+            document.getElementById("checkbox-number").innerText = checkBoxNumber;
+            event.target.disabled = true;
+            
+            if(taskNumber < 1 ){
+                alert("congratulations you have completed all the current task")
 
+                
+            }
+            
+            const currentDate = document.getElementById("current-date").innerText;
+
+            const container = document.getElementById("activity-history");
+            const div = document.createElement("div");
+            div.innerHTML = `
+            <div class="m-4 p-3 bg-white rounded-md">You have completed the task at ${currentDate}
+
+            `
+            container.appendChild(div)
+        })
+    })
+})
 
